4847bf66f533ebd82cecc1bfed4ec6e0c65a9020
Sefa Saraç <srcsfysf@gmail.com>
2024-08-07 21:28:00 +0300

    Add files via upload

---
+++ Kutuphane/Kutuphane/Kitap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane
+{
+    public class Kitap   // Kitap adında bir class oluşturuyoruz
+    {
+        // Property tanımlamaları       
+        public string KitapAdi { get; set; }
+        public string YazarAdi { get; set; }
+        public string YazarSoyadi {  get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayınevi { get; set; }
+        public DateTime KayitTarihi { get; private set; }
+
+        // Default Constructor
+        public Kitap()
+        {
+            KayitTarihi = DateTime.Now;
+            Console.WriteLine("Kitap nesnesi oluşturuldu.");
+        }
+
+        // Alternatif Constructor
+        public Kitap(string kitapAdi, string yazarAdi, string yazarSoyadi, int sayfaSayisi, string yayinevi)
+        {
+            KitapAdi = kitapAdi;
+            YazarAdi = yazarAdi;
+            YazarSoyadi = yazarSoyadi;
+            SayfaSayisi = sayfaSayisi;
+            Yayınevi = yayinevi;
+            KayitTarihi = DateTime.Now;
+            Console.WriteLine("Kitap nesnesi oluşturuldu.");
+        }
+
+        // Kitap bilgilerini yazdırmak için bir metot
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Kitap Adı: {KitapAdi}, Yazar Adı: {YazarAdi}, Yazar Sayadı: {YazarSoyadi}, Sayfa Sayısı: {SayfaSayisi}, Yayınevi: {Yayınevi}, Kayıt Tarihi: {KayitTarihi}");
+        }
+    }
+}
---
+++ Kutuphane/Kutuphane/Program.cs
@@ -0,0 +1,21 @@
+﻿using Kutuphane;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main()
+    {
+        // Default Constructor ile kitap nesnesi oluşturma
+        Kitap kitap1 = new Kitap();
+        kitap1.KitapAdi = "Aylin";
+        kitap1.YazarAdi = "Ayşe";
+        kitap1.YazarSoyadi = "Kulin";
+        kitap1.SayfaSayisi = 398;
+        kitap1.Yayınevi = "Remzi Kitabevi";
+        kitap1.BilgileriYazdir();
+
+        // Alternatif Constructor ile kitap nesnesi oluşturma
+        Kitap kitap2 = new Kitap("Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+        kitap2.BilgileriYazdir();
+    }
+}
